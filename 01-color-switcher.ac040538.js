ea1aef371730f5290692bfc3cea23285d2aecbf3
Tkachenko-Yuriy <102075946+Tkachenko-Yuriy@users.noreply.github.com>
2022-11-01 10:47:09 +0000

    Deploying to gh-pages from @ Tkachenko-Yuriy/goit-js-hw-09@b6f347df4e45b7cae1b0075bfca7141ef7ec2e22 🚀

---
+++ 01-color-switcher.ac040538.js
@@ -0,0 +1,2 @@
+const t={btnStartEl:document.querySelector("button[data-start]"),btnStopEl:document.querySelector("button[data-stop]"),bodyEl:document.body};let e=null;t.btnStartEl.addEventListener("click",(()=>{e=setInterval((()=>{t.bodyEl.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.btnStartEl.disabled=!0})),t.btnStopEl.addEventListener("click",(()=>{clearInterval(e),t.btnStartEl.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.ac040538.js.map
