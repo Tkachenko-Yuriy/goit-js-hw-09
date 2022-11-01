b6f347df4e45b7cae1b0075bfca7141ef7ec2e22
Tkachenko_Yuriy <uvt86@ukr.net>
2022-11-01 12:46:43 +0200

    add 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const refs = {
+  btnStartEl: document.querySelector('button[data-start]'),
+  btnStopEl: document.querySelector('button[data-stop]'),
+  bodyEl: document.body,
+}
+let intervalId = null;
 
+refs.btnStartEl.addEventListener('click',()=>{
+   intervalId = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();},1000)
+    refs.btnStartEl.disabled = true;
+})
+
+refs.btnStopEl.addEventListener('click',()=> {
+    clearInterval(intervalId)
+    refs.btnStartEl.disabled = false;
+})
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
